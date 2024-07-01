dd3ec8f0ab6a6edc46f0041540a41ad77ee8e90c
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-01 12:29:39 +0530

    reading a file for /fileread route

--- indexx.js
+++ indexx.js
@@ -1,23 +1,34 @@
+const fs = require("fs");
 const http = require("http");
 
-const server = http.createServer((req, res) =>{
-    res.setHeader("Content-Type", "text/html");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
 
-
-    if(req.url === "/login"){
-        res.write("<html> <head> <title> Node Js </title> </head> <body> <h1> Login </h1> </body> </html>");
-        res.end();
-        return;
-    }
-
-    res.write("<html> <head> <title> Node Js </title> </head> <body> <h1> Hello I am narasimha </br> and this is the things we do </h1> </body> </html>");
+  if (req.url === "/login") {
+    res.write(
+      "<html> <head> <title> Node Js </title> </head> <body> <h1> Login </h1> </body> </html>"
+    );
     res.end();
-})
+    return;
+  }
+
+  if (req.url === "/fileread") {
+    const data = fs.readFileSync("hello.txt", "utf8");
+    res.write(data);
+    res.end();
+    return;
+  }
+
+  res.write(
+    "<html> <head> <title> Node Js </title> </head> <body> <h1> Hello I am narasimha </br> and this is the things we do </h1> </body> </html>"
+  );
+  res.end();
+});
 
 const port = 3000;
 
 const host = "localhost";
 
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
